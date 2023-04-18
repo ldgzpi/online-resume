6ac63cc0b1e6dc395cafe1f00df5eb1a6596f6dd
ldgzpi <pilarledesmagonzalez@gmail.com>
2023-04-18 14:36:35 -0300

    Template update

---
+++ js/main.js
@@ -0,0 +1,225 @@
+// Functions for each action-command.
+
+function help(){
+  return "<h2><span style=\"color:#eb926d;\">Help:</span></h2><table>\
+  <tr>\
+    <td>all</td>\
+    <td>Give me the complete picture</td>\
+  </tr>\
+  <tr>\
+    <td>about</td>\
+    <td>Everything you ever wanted to know about John Smith</td>\
+  </tr>\
+  <tr>\
+    <td>contact</td>\
+    <td>How to get in touch with John Smith</td>\
+  </tr>\
+  <tr>\
+    <td>links</td>\
+    <td>See what links John Smith wants you to click on</td>\
+  </tr>\
+  <tr>\
+    <td>skills</td>\
+    <td>Get to know how good John Smith is at stuff</td>\
+  </tr>\
+  <tr>\
+    <td>cv</td>\
+    <td>Download John Smith's CV</td>\
+  </tr>\
+  <tr>\
+    <td>rickroll</td>\
+    <td>Never Gonna Give You Up</td>\
+  </tr></table>";
+}
+
+function contact(){
+  return "<h2><span style=\"color:#cc6666;\">Contact:</span></h2><table>\
+  <tr>\
+    <td>Email</td>\
+    <td><a href=\"mailto:john.smith@emailprovider.com\">john.smith@emailprovider.com</a></td>\
+  </tr>\
+  <tr>\
+    <td>Telephone</td>\
+    <td><a href=\"tel:+45-657-56234\">+45-657-56234</a></td>\
+  </tr>\
+  <tr>\
+    <td>Address</td>\
+    <td>John Smith</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>205 Autumn Street</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>5472</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>London</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>UK</td>\
+  </tr></table>";
+}
+
+function skills(){
+  return "<h2><span style=\"color:#81a2be;\">Skills:</span></h2><table>\
+  <tr>\
+    <td>Photoshop</td>\
+    <td>▰▰▰▰▰▰▱▱▱▱ 60%</td>\
+  </tr>\
+  <tr>\
+    <td>HTML</td>\
+    <td>▰▰▰▰▰▱▱▱▱▱ 50%</td>\
+  </tr>\
+  <tr>\
+    <td>Premiere Pro</td>\
+    <td>▰▰▰▰▰▰▰▰▱▱ 80%</td>\
+  </tr>\
+  <tr>\
+    <td>Rickrolling</td>\
+    <td>▰▰▰▰▰▰▰▰▰▰ 100%</td>\
+  </tr></table>";
+}
+
+function about(){
+  return "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+}
+
+function cv(){
+  return "<span style=\"color:#8abeb7;\"><h2>CV:</h2></span><p>\tDownload from <a href=\"src/cv.pdf\" target=\"_blank\" style=\"text-decoration: underline;\">here</a>.</p>";
+}
+
+function credits(){
+  return "<p>Built by <a href=\"https://www.github.com/CedArctic\" target=\"_blank\"><i class=\"fab fa-github\"></i> CedArctic</a></p>";
+}
+
+function links(){
+  return "<span style=\"color: #b5bd68;\"><h2>Links:</h2></span><ul>\
+  <li><a href=\"https://www.linkedin.com\" target=\"_blank\"><i class=\"fab fa-linkedin\"></i> LinkedIn</a></li>\
+  <li><a href=\"https://www.github.com\" target=\"_blank\"><i class=\"fab fa-github\"></i> Github</a></li>\
+  <li><a href=\"https://www.twitter.com\" target=\"_blank\"><i class=\"fab fa-twitter\"></i> Twitter</a></li>\
+  <li><a href=\"https://www.facebook.com\" target=\"_blank\"><i class=\"fab fa-facebook\"></i> Facebook</a></li>\
+  </ul>";
+}
+
+function rickroll(){
+  return "<p>We're no strangers to love<br>\
+    You know the rules and so do I<br>\
+    A full commitment's what I'm thinking of<br>\
+    You wouldn't get this from any other guy<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    And if you ask me how I'm feeling<br>\
+    Don't tell me you're too blind to see<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    (Ooh, give you up)<br>\
+    (Ooh, give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you</p>";
+}
+
+// Main Function
+function commandProcessor(e){
+
+  //Check if the enter key is pressed
+  if(e.keyCode == 13){
+
+    //Clear the area where info will be printed
+    document.getElementById('injected').innerHTML= "";
+
+    //Get user input
+    var txtInput = document.getElementById('txtBox').value;
+
+    //Select what info to print according to command
+    if(txtInput == "help"){
+      document.getElementById('injected').innerHTML=help();
+    }else if (txtInput=="all") {
+      document.getElementById('injected').innerHTML=about() + "\n\n\n" + skills() + "\n\n\n" + links() + "\n\n\n" + contact() + "\n\n\n" + cv();
+    }else if (txtInput == "about") {
+      document.getElementById('injected').innerHTML=about();
+    }else if (txtInput == "contact") {
+      document.getElementById('injected').innerHTML=contact();
+    }else if (txtInput == "cv") {
+      document.getElementById('injected').innerHTML=cv();
+    }else if (txtInput=="skills") {
+      document.getElementById('injected').innerHTML=skills();
+    }else if (txtInput=="links") {
+      document.getElementById('injected').innerHTML=links();
+    }else if (txtInput == "rickroll") {
+      var win = window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
+      win.focus();
+      document.getElementById('injected').innerHTML=rickroll();
+    }else if (txtInput == "credits") {
+      document.getElementById('injected').innerHTML=credits();
+    }else{
+      document.getElementById('injected').innerHTML = help();
+    }
+
+    //Clear input text box
+    document.getElementById('txtBox').value= "";
+  }
+}
